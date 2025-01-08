da74d9d8ee8546940a00a6d8f96cb0d274f365c9
Cameron Ellis <cameronellis71@gmail.com>
2025-01-08 00:52:14 -0800

    random-select-email-templates: Moved email template out of else statement to a helper function

--- src/App.js
+++ src/App.js
@@ -3,6 +3,28 @@ import './App.css';
 
 let templateToUse = Math.floor(Math.random() * 10) + 1
 
+function generateEmail(answers) {
+  return `
+Got it, here's what I was able to come up with for you:
+
+Dear Team,
+
+Today we are making some significant changes to the structure of our team and the design of our
+organization which will result in approximately ${answers[0]} team members leaving ${answers[1]}.
+We believe these changes are necessary because of ${answers[2]} and ${answers[3]} internet memes.
+
+We know our unique culture, and our values of being kind, smart, and creative, are a reflection
+of the amazing people who work at ${answers[1]}. It pains me that many people I have deeply
+enjoyed working with, who I know firsthand are extremely talented, will no longer be members of
+our team at ${answers[1]}. We are infinitely grateful for your contributions, your hard work,
+and your ambition to make a positive impact in the world.
+
+${answers[4]}
+
+Please let me know if you'd like me to create another layoff email for you.
+  `;
+}
+
 // Predefined list of questions with unique suggestions
 const questions = [
   { 
@@ -10,22 +32,28 @@ const questions = [
     "I'll ask you a few questions and all you have to do is provide an answer. I'll do the rest." +
     "\n\nIf you can't think of anything, use one of the suggestions below" +
     "\n\nTo start, can you give me a number?", 
-    suggestions: ["10,000", "1,000", "5,000"] 
+    suggestions: ["10,000", "1,000", "5,000"],
+    answerKey: "numEmployeesLaidOff",
   },
   { 
     question: "Can you give me a company name?",
-    suggestions: ["Snup", "Macrosoft", "TokTik"] },
+    suggestions: ["Snup", "Macrosoft", "TokTik"],
+    answerKey: "companyName",
+  },
   { 
     question: "Can you give me a reason for the layoff?",
-    suggestions: ["conflict in the Middle East", "macroeconomic headwinds", "Mercury in retrograde"] 
+    suggestions: ["conflict in the Middle East", "macroeconomic headwinds", "Mercury in retrograde"],
+    answerKey: "layoffReason",
   },
   { 
     question: "Can you give me a number?",
-    suggestions: ["10,000", "1,000", "5,000"]
+    suggestions: ["10,000", "1,000", "5,000"],
+    answerKey: "numInternetMemes",
   },
   { 
     question: "Can you give me a name?",
-    suggestions: ["Evan", "Jeff", "Sundar"]
+    suggestions: ["Evan", "Jeff", "Sundar"],
+    answerKey: "layoffEmailAuthor"
   }
 ];
 
@@ -67,22 +95,9 @@ function App() {
     } else {
       console.log("templateToUse: ", templateToUse)
       // If no more questions, display the summary
+      // Store the last question in the answer list
       const answers = responses.concat(inputText);
-
-      const generatedEmail = "Got it, here's what I was able to come up with for you:\n\n" +
-      "Dear Team,\n\n" +
-      "Today we are making some significant changes to the structure of our team and the design of our organization " +
-      "which will result in aproximately " + answers[0] + " team members leaving " + answers[1] + ". We believe " +
-      "these changes are necessary because of " + answers[2] + " and " + answers[3] + " internet memes." +
-      "\n\n" +
-      "We know our unique culture, and our values of being kind, smart, and creative, are a reflection of the " +
-      "amazing people who work at " + answers[1] + ". It pains me that many people I have deeply enjoyed working" +
-      " with, who I know firsthand are extremely talented, will no longer be members of our team at " + answers[1] + ". We are" +
-      " infinitely grateful for your contributions, your hard work, and your ambition to make a positive impact" +
-      " in the world." +
-      "\n\n" +
-      answers[4] +
-      "\n\n\nPlease let me know if you'd like me to create another layoff email for you"
+      const generatedEmail = generateEmail(answers)
 
       setMessages((prev) => [
         ...prev,
